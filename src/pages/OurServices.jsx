aebd2226dff1b1483f1788d294e0ffeddb383e94
BenMabel <itzmabella103@gmail.com>
2023-03-08 06:48:40 +0100

    added a service

--- src/pages/OurServices.jsx
+++ src/pages/OurServices.jsx
@@ -19,20 +19,20 @@ const OurServices = () => {
 
                 <div style={{ display: 'flex', marginTop: 20, marginBottom: 20, alignItems: 'center', justifyContent: 'space-between' }}>
                     <div className='card' >
-                        <div className='title'> Lorem Ipsum</div>
+                        <div className='title'> Local Food Hub</div>
                         <div style={{ width: 90, height: 10, background: 'red', borderRadius: 10, marginTop: 20, marginBottom: 30 }}></div>
-                        <div className='desc'>Lorem ipsum lorem ipsumLorem ipsum lorem ipsum Lorem ipsum lorem ipsumLorem ipsum lorem ipsum. Lorem  ipsum Lorem ipsum lorem ipsumLorem ipsum lorem ipsum.</div>
+                        <div className='desc'>We provide a service connecting you to your favourite local food restaurant. A platform where local food sellers can increase their sales and also enjoy a grant priviledge for improving their businesses through donations received.</div>
                     </div>
                     <div className='card' >
-                        <div className='title'> Lorem Ipsum</div>
+                        <div className='title'> Express Delivery</div>
                         <div style={{ width: 90, height: 10, background: 'red', borderRadius: 10, marginTop: 20, marginBottom: 30 }}></div>
-                        <div className='desc'>Lorem ipsum lorem ipsumLorem ipsum lorem ipsum Lorem ipsum lorem ipsumLorem ipsum lorem ipsum. Lorem  ipsum Lorem ipsum lorem ipsumLorem ipsum lorem ipsum.</div>
+                        <div className='desc'>We bring comfort to local food lovers by offering them an ordering service where they can get quick delivery of meals from any Buka of their choice and also track it's process until arrival.</div>
 
                     </div>
                     <div className='card' >
-                        <div className='title'> Lorem Ipsum</div>
+                        <div className='title'> Quality Control</div>
                         <div style={{ width: 90, height: 10, background: 'red', borderRadius: 10, marginTop: 20, marginBottom: 30 }}></div>
-                        <div className='desc'>Lorem ipsum lorem ipsumLorem ipsum lorem ipsum Lorem ipsum lorem ipsumLorem ipsum lorem ipsum. Lorem  ipsum Lorem ipsum lorem ipsumLorem ipsum lorem ipsum.</div>
+                        <div className='desc'>Ensuring that users get quality local food is our priority. With our structured system, we ensure that our partners are offering the best quality from an healthy environment while we investigate negative feedbacks.</div>
                     </div>
                 </div>
                 <Link className='loginBtn' style={{ width: '15%', textAlign: 'center', marginBottom: 30, marginTop: 20 }} to='#'>Make a donation</Link>
